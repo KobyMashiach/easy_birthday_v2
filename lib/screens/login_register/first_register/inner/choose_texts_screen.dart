e254b621f9bcb1611b5932cc01640c17045d968f
Koby Mashiach <kobym@algoretail.io>
2024-09-17 15:33:58 +0300

    choose_text_screens

--- lib/screens/login_register/first_register/inner/choose_texts_screen.dart
+++ lib/screens/login_register/first_register/inner/choose_texts_screen.dart
@@ -4,34 +4,117 @@ import 'package:easy_birthday/i18n/strings.g.dart';
 import 'package:easy_birthday/widgets/general/bottom_navigation_bars/app_buttons_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class ChooseTextsScreen extends StatelessWidget {
-  final VoidCallback onContinue;
-  final VoidCallback onPrevious;
+class ChooseTextsScreen extends StatefulWidget {
   const ChooseTextsScreen(
       {super.key, required this.onContinue, required this.onPrevious});
+
+  final VoidCallback onContinue;
+  final VoidCallback onPrevious;
+
+  @override
+  State<ChooseTextsScreen> createState() => _ChooseTextsScreenState();
+}
+
+class _ChooseTextsScreenState extends State<ChooseTextsScreen> {
+  int? expandedIndex;
+  final bool male = checkIfMaleGender(globalPartnerUser!.gender);
 
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
-      onPopInvokedWithResult: (didPop, result) => onPrevious(),
+      onPopInvokedWithResult: (didPop, result) => widget.onPrevious(),
       child: Scaffold(
         body: Center(
           child: Column(
             children: [
-              Text(globalPartnerUser!.gender),
+              Expanded(
+                child: titlesListView(),
+              ),
             ],
           ),
         ),
         bottomNavigationBar: AppButtonsBottomNavigationBar(
           activeButtonText: t.continue_,
           activeButtonOnTap: () async {
-            onContinue.call();
+            widget.onContinue.call();
           },
           inactiveButtonText: t.back,
-          inactiveButtonOnTap: onPrevious,
+          inactiveButtonOnTap: widget.onPrevious,
         ),
       ),
+    );
+  }
+
+  ListView titlesListView() {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: ListTile(
+                      title: Text('Title $index'),
+                      onTap: () {
+                        setState(() {
+                          if (expandedIndex == index) {
+                            expandedIndex = null;
+                          } else {
+                            expandedIndex = index;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: AnimatedSwitcher(
+                      duration: Duration(milliseconds: 300),
+                      transitionBuilder:
+                          (Widget child, Animation<double> animation) {
+                        return SizeTransition(
+                          sizeFactor: animation,
+                          axis: Axis.horizontal,
+                          child: child,
+                        );
+                      },
+                      child: expandedIndex == index
+                          ? Container(
+                              key: ValueKey<int>(index),
+                              height: 100,
+                              child: horizontalListViews(),
+                            )
+                          : SizedBox.shrink(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  ListView horizontalListViews() {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: 5,
+      itemBuilder: (context, i) {
+        return Container(
+          width: 100,
+          margin: EdgeInsets.symmetric(horizontal: 5),
+          color: Colors.blue,
+          child: Center(
+            child: Text('Item $i', style: TextStyle(color: Colors.white)),
+          ),
+        );
+      },
     );
   }
 }
